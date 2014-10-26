e49ad7fbce233b025ee3d25c9733b58015a487fa
Thong B. Tran <baocongchen@yahoo.com>
2014-10-26 18:52:53 +0900

    Update Intro_to_Interactive_Programming.py

--- Intro_to_Interactive_Programming.py
+++ Intro_to_Interactive_Programming.py
@@ -309,3 +309,14 @@ while len(numbers) > 0:
         if num_divide%numbers[0]==0:
             numbers.remove(num_divide)
 print len(results)
+
+#Quiz 6b 8
+year = 1
+slow_wumpuses = 1000
+fast_wumpuses = 1
+while slow_wumpuses > fast_wumpuses:
+    slow_wumpuses += slow_wumpuses * 2 * 60/100
+    fast_wumpuses += fast_wumpuses * 2 * 70/100
+    year += 1
+print year - 1
+
